f929ca6d22c7ef1f21d707a7efccf10416a97c32
Andrew <andrew@nomstock.com>
2017-11-24 08:51:50 -0800

    initial commit from wwpbi template

---
+++ includes/class-wp-rsync-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://github.com/ajdruff/
+ * @since      1.0.0
+ *
+ * @package    Wp_Rsync
+ * @subpackage Wp_Rsync/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Rsync
+ * @subpackage Wp_Rsync/includes
+ * @author     Andrew Druffner <andrew_at_nomstock.com>
+ */
+class Wp_Rsync_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-wp-rsync-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://github.com/ajdruff/
+ * @since      1.0.0
+ *
+ * @package    Wp_Rsync
+ * @subpackage Wp_Rsync/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Rsync
+ * @subpackage Wp_Rsync/includes
+ * @author     Andrew Druffner <andrew_at_nomstock.com>
+ */
+class Wp_Rsync_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
